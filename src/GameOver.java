1882f7c989422ae7545edcae2376bedade6b4147
GdanskaProgramistka <emug@vp.pl>
2021-04-17 20:34:25 +0200

    more players option + tie option

--- src/GameOver.java
+++ src/GameOver.java
@@ -3,8 +3,22 @@ public class GameOver {
     public static boolean gameOver;
 
     public static void checkWinner() {
-        System.out.println("Game Over. The winner is " + Players.countWins() + ". Congratulations!");
-        System.exit(0);
+        int countTie = 0;
+        for (int i = 1; i < Players.createdPlayersList.size(); i++) {
+            if (Players.createdPlayersList.get(0).win == (Players.createdPlayersList.get(i).win)) {
+                countTie++;
+            }
+        }
+        if (countTie == 0) {
+            System.out.println("Game Over. The winner is " + Players.countWins() + ". Congratulations!");
+            System.exit(0);
+        } else {
+            System.out.println("There is a tie between: ");
+            for(int j = 0; j <= countTie; j++){
+                System.out.println(Players.createdPlayersList.get(j));
+            }
+            System.exit(0);
+        }
     }
 
 //    public static void checkWinner(Player player1, Player player2) {
